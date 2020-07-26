01200718c0317ec984d4c9433da17fcc073dda49
mathlun <matheuslunadev@gmail.com>
2020-07-26 13:28:56 -0300

    configure middleware

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const body_parser_1 = require("body-parser");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+app.use(body_parser_1.json());
+app.use(body_parser_1.urlencoded({ extended: false }));
 app.get('/', (_, res) => {
-    res.send('ConnectJobs');
+    let users = [
+        { name: "matheus", age: 23 },
+        { name: "sheila", age: 23 },
+    ];
+    res.json(users);
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import * as express from 'express';
 import { Application, Response } from 'express';
+import { json, urlencoded } from 'body-parser';
+import * as cors from 'cors';
 
 const app: Application = express();
 
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
 app.get('/', (_, res: Response) => {
-	res.send('ConnectJobs');
+	let users = [
+		{ name: "matheus", age: 23 },
+		{ name: "sheila", age: 23 },
+	];
+	res.json(users);
 })
 
 export default app;
